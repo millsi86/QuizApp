7c3eb0c62febebce5173b16a28a339a355df65cc
millsi86 <tommills86@gmail.com>
2017-03-07 10:11:35 +0000

    Added logic for calculating the interlock size Added logic for calculating the workstation size from Interlock and Internal Sizes

--- app/src/main/java/com/example/android/quizapp/MainActivity.java
+++ app/src/main/java/com/example/android/quizapp/MainActivity.java
@@ -9,16 +9,23 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import static com.example.android.quizapp.R.id.IntSize1;
+import static com.example.android.quizapp.R.id.IntSize2;
+import static com.example.android.quizapp.R.id.IntSize3;
+import static com.example.android.quizapp.R.id.IntSize4;
+import static com.example.android.quizapp.R.id.IntSize5;
 import static com.example.android.quizapp.R.id.Radio_T75;
 import static com.example.android.quizapp.R.id.WorkingCapacity;
 import static com.example.android.quizapp.R.id.WorkingCapacityText;
 import static com.example.android.quizapp.R.id.radioGroup1;
+import static com.example.android.quizapp.R.id.radioGroup2;
 
 
 public class MainActivity extends AppCompatActivity {
 
     private RadioGroup Group1;
-    private RadioButton Flask, Well, Plate;
+    private RadioGroup Group2;
+
 
 
 
@@ -27,25 +34,79 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Group1 = (RadioGroup) findViewById(radioGroup1);
+        Group2 = (RadioGroup) findViewById(radioGroup2);
     }
 
     public void submit (View view){
         String CapType = getRadio1ID(view);
         EditText CapacityId = (EditText) findViewById(WorkingCapacity);
         Integer Volume = Integer.parseInt(CapacityId.getText().toString());
-        Integer WorkstationSize;
+        Integer InternalSize;  // Numerical value for workstation size
+        Integer InterlockSize;  // Numerical value for interlock size
+        String WorkstationSize;
 
-        if(CapType == "Flask"){
-            WorkstationSize = FlaskWorkstationSize(Volume);
-        }
+        if(CapType.equals("Flask")){InternalSize = FlaskWorkstationSize(Volume);}
+        else if(CapType.equals("Well")){InternalSize = WellWorkstationSize(Volume);}
+        else {InternalSize = DishWorkstationSize(Volume);}
+        InterlockSize = InterlockSizeSub(view);
+        WorkstationSize = CalculateWorkstationSize(InternalSize, InterlockSize);
 
-        else if(CapType == "Well"){
-            WorkstationSize = WellWorkstationSize(Volume);
-        }
 
-        else {WorkstationSize = DishWorkstationSize(Volume);}
+
+
 
     }
+
+    private String CalculateWorkstationSize (Integer InternalSize, Integer InterlockSize){
+        String WorkstationSize;
+        if(InternalSize == 1){
+            if(InterlockSize == 1){WorkstationSize = "BB";}
+            else if(InterlockSize == 2){WorkstationSize = "BB+";}
+            else if(InterlockSize == 3){WorkstationSize = "N400";}
+            else if(InterlockSize == 4){WorkstationSize = "N500";}
+            else if(InterlockSize == 5){WorkstationSize = "N1000";}
+            else {WorkstationSize = "SCI Sgl";}
+        }
+        else if(InternalSize == 2) {
+            if(InterlockSize <= 3) {WorkstationSize = "N400";}
+            else if(InterlockSize == 4) {WorkstationSize = "N500";}
+            else if(InterlockSize == 5) {WorkstationSize = "N1000";}
+            else {WorkstationSize = "SCI Sgl";}
+        }
+        else if(InternalSize == 3){
+            if(InterlockSize <= 5){WorkstationSize = "N500";}
+            else{WorkstationSize = "SCI Sgl";}
+        }
+        else if(InternalSize == 4) {
+            if (InterlockSize <= 5) {WorkstationSize = "N500";}
+            else{WorkstationSize = "SCI Dual";}
+        }
+        else{WorkstationSize = "SCI Dual";}
+
+        Toast.makeText(this, WorkstationSize, Toast.LENGTH_SHORT).show();
+
+        return WorkstationSize;
+    } // Calculates the Workstation size based on Internal and Interlock Volumes
+
+    private Integer InterlockSizeSub (View view){
+        Integer IntSize;
+        RadioButton Button1 = (RadioButton) findViewById(IntSize1);
+        RadioButton Button2 = (RadioButton) findViewById(IntSize2);
+        RadioButton Button3 = (RadioButton) findViewById(IntSize3);
+        RadioButton Button4 = (RadioButton) findViewById(IntSize4);
+        RadioButton Button5 = (RadioButton) findViewById(IntSize5);
+
+        int selectedId = Group2.getCheckedRadioButtonId();
+
+        if(selectedId == Button1.getId()){IntSize = 1;}
+        else if(selectedId == Button2.getId()){IntSize = 2;}
+        else if(selectedId == Button3.getId()){IntSize = 3;}
+        else if(selectedId == Button4.getId()){IntSize = 4;}
+        else if(selectedId == Button5.getId()){IntSize = 5;}
+        else {IntSize = 6;}
+
+        return IntSize;
+    } // Gets Interlock Size from RadioGroup2
 
     private Integer FlaskWorkstationSize(Integer MainVolume){
         Integer Volume1 = 77;
@@ -56,7 +117,7 @@ public class MainActivity extends AppCompatActivity {
         Integer workstationSize;
 
         return workstationSize = WorkstationVolumeCalc(MainVolume, Volume1, Volume2, Volume3, Volume4);
-    }
+    } // Specifies the Internal Volumes available for Workstations for T75 Flasks
 
     private Integer WellWorkstationSize(Integer MainVolume){
         Integer Volume1 = 199;
@@ -67,7 +128,7 @@ public class MainActivity extends AppCompatActivity {
         Integer workstationSize;
 
         return workstationSize = WorkstationVolumeCalc(MainVolume, Volume1, Volume2, Volume3, Volume4);
-    }
+    } // Specifies the Internal Volumes available for Workstations for 96 Well Plates
 
     private Integer DishWorkstationSize(Integer MainVolume){
         Integer Volume1 = 180;
@@ -78,7 +139,7 @@ public class MainActivity extends AppCompatActivity {
         Integer workstationSize;
 
         return workstationSize = WorkstationVolumeCalc(MainVolume, Volume1, Volume2, Volume3, Volume4);
-    }
+    } // Specifies the Internal Volumes available for Workstations for 90mm Petri-Dishes
 
     private Integer WorkstationVolumeCalc(Integer Volume, Integer Size1, Integer Size2, Integer Size3, Integer Size4){
         Integer workstationSize;
@@ -87,32 +148,27 @@ public class MainActivity extends AppCompatActivity {
         else if(Volume <= Size3){ workstationSize = 3; }
         else if(Volume <= Size4){ workstationSize = 4; }
         else{workstationSize = 5;}
-        Toast.makeText(this, workstationSize.toString(), Toast.LENGTH_SHORT).show();
+        // Toast.makeText(this, workstationSize.toString(), Toast.LENGTH_SHORT).show();
         return workstationSize;
-    }
+    } // Calculates the required workstation volume size for all Media types. Returns an Integer relating to the size
 
     private String getRadio1ID (View view){
         String CapacityType;
+        RadioButton Flask, Well; // ,Plate
         Flask = (RadioButton) findViewById(Radio_T75);
         Well = (RadioButton) findViewById(R.id.Radio_96well);
-        Plate = (RadioButton) findViewById(R.id.Radio_90mm);
+        // Plate = (RadioButton) findViewById(R.id.Radio_90mm);
 
         int selectedId = Group1.getCheckedRadioButtonId();
 
-        if(selectedId == Flask.getId()){
-            CapacityType = "Flask";
-        }
-        else if(selectedId == Well.getId()){
-            CapacityType = "Well";
-        }
-        else {
-            CapacityType = "Plate";
-        }
+        if(selectedId == Flask.getId()){CapacityType = "Flask";}
+        else if(selectedId == Well.getId()){CapacityType = "Well";}
+        else {CapacityType = "Plate";}
 
         //Toast.makeText(this, CapacityType, Toast.LENGTH_SHORT).show();  /Line used for testing Group1 Radio Button section
 
         return CapacityType;
-    }
+    } // Checks RadioGrou1 for the media type selected and returns the type as a string
 
 
     // Declarations to get radio buttons changing when the corresponting image is selected:
@@ -219,27 +275,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void T75IntBB (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize1);
+        TextView Text = (TextView) findViewById(IntSize1);
         Text.setText(R.string.IntBBFlask);
     }
 
     public void T75IntBBPlus (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize2);
+        TextView Text = (TextView) findViewById(IntSize2);
         Text.setText(R.string.IntBBplusFlask);
     }
 
     public void T75IntNuvoLrg (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize3);
+        TextView Text = (TextView) findViewById(IntSize3);
         Text.setText(R.string.IntNuvoSmlFlask);
     }
 
     public void T75IntNuvoSml (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize4);
+        TextView Text = (TextView) findViewById(IntSize4);
         Text.setText(R.string.IntNuvoLgeFlask);
     }
 
     public void T75IntNuvoDual (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize5);
+        TextView Text = (TextView) findViewById(IntSize5);
         Text.setText(R.string.IntNuvoDualFlask);
     }
 
@@ -249,27 +305,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void WellIntBB (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize1);
+        TextView Text = (TextView) findViewById(IntSize1);
         Text.setText(R.string.IntBBWell);
     }
 
     public void WellIntBBPlus (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize2);
+        TextView Text = (TextView) findViewById(IntSize2);
         Text.setText(R.string.IntBBplusWell);
     }
 
     public void WellIntNuvoSml (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize3);
+        TextView Text = (TextView) findViewById(IntSize3);
         Text.setText(R.string.IntNuvoSmlWell);
     }
 
     public void WellIntNuvoLge (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize4);
+        TextView Text = (TextView) findViewById(IntSize4);
         Text.setText(R.string.IntNuvoLgeWell);
     }
 
     public void WellIntNuvoDual (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize5);
+        TextView Text = (TextView) findViewById(IntSize5);
         Text.setText(R.string.IntNuvoDualWell);
     }
 
@@ -279,27 +335,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void PlateIntBB (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize1);
+        TextView Text = (TextView) findViewById(IntSize1);
         Text.setText(R.string.IntBBPlate);
     }
 
     public void PlateIntBBPlus (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize2);
+        TextView Text = (TextView) findViewById(IntSize2);
         Text.setText(R.string.IntBBplusPlate);
     }
 
     public void PlateIntNuvoSml (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize3);
+        TextView Text = (TextView) findViewById(IntSize3);
         Text.setText(R.string.IntNuvoSmlPlate);
     }
 
     public void PlateIntNuvoLge (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize4);
+        TextView Text = (TextView) findViewById(IntSize4);
         Text.setText(R.string.IntNuvoLgePlate);
     }
 
     public void PlateIntNuvoDual (View view){
-        TextView Text = (TextView) findViewById(R.id.IntSize5);
+        TextView Text = (TextView) findViewById(IntSize5);
         Text.setText(R.string.IntNuvoDualPlate);
     }
 
